89e5f23483b06a4d36d311c6a1749073a2c21fc1
Matt Levonian <elevonian@gmail.com>
2016-07-15 16:50:33 -0700

    Added player prefab

---
+++ Assets/Player.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+    public float speed = 2;
+	void Update () {
+        transform.position += Input.GetAxis("Horizontal") * Vector3.right * speed * Time.deltaTime;
+        transform.position += Input.GetAxis("Vertical") * Vector3.up * speed * Time.deltaTime;
+    }
+}
