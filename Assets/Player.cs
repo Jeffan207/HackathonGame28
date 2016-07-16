bdfb9687f31c8934a5e76d8792c7f124ea35ab4a
Matt Levonian <elevonian@gmail.com>
2016-07-15 18:28:04 -0700

    Added a Cmd/ClientRpc pair

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,12 +4,41 @@ using UnityEngine.Networking;
 
 public class Player : NetworkBehaviour {
     public float speed = 2;
+    
+    public float maxSpeed = 20;
+
+    public float minRadius = 1;
+    public float force = 5;
 
 	void Update () {
         if (this.isLocalPlayer)
         {
-            transform.position += Input.GetAxis("Horizontal") * Vector3.right * speed * Time.deltaTime;
-            transform.position += Input.GetAxis("Vertical") * Vector3.up * speed * Time.deltaTime;
+            GetComponent<Rigidbody2D>().AddForce(Input.GetAxis("Horizontal") * Vector3.right * speed * Time.deltaTime + 
+                                                 Input.GetAxis("Vertical")   * Vector3.up    * speed * Time.deltaTime);
+            GetComponent<Rigidbody2D>().velocity = Vector3.ClampMagnitude(GetComponent<Rigidbody2D>().velocity, maxSpeed);
+        }
+        if(Input.GetButton("Jump"))
+        {
+            CmdBounce();
+        }
+    }
+
+    [Command]
+    void CmdBounce()
+    {
+        Debug.Log("COMMAND Bounce");
+        RpcBounce();
+    }
+
+    [ClientRpc]
+    void RpcBounce()
+    {
+        Debug.Log("CLIENTRPC Bounce");
+        foreach(Player player in FindObjectsOfType<Player>())
+        {
+            Vector3 delta = (transform.position - player.transform.position);
+            float amount = Mathf.Max(minRadius, delta.magnitude);
+            player.GetComponent<Rigidbody2D>().AddForce(-delta.normalized * 1f/amount * force);
         }
     }
 }
